260d2741a4b4b446965393f4abf5c6623c5be309
James Becker <jamsbec@gmail.com>
2016-04-22 12:12:44 +0700

    Update and rename sumAll.js to BF-INT-02-sumAll.js

--- BF-INT-02-sumAll.js
+++ BF-INT-02-sumAll.js
@@ -5,16 +5,20 @@ function sumAll(arr) {
     return a - b;
   });
   
+  //set new empty array
   var newArr = [];
   
+  //to new array, push values from lower number to higher number
   for (i = arr[0]; i <= arr[1]; i++) {
     newArr.push(i);
-  }
+  };
   
+  //set arr equal to reduction of new array by sum
   arr = newArr.reduce(function(a,b) {
     return a + b;
   });
   
+  //return array
   return arr;
 
 }
